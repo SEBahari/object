3a4a63d6dfc0422322f135bf258d235829a7ba0c
ehsan <ebaharinasab.eb@gmail.com>
2021-04-24 23:52:00 +0430

    start hw3

--- hw3.js
+++ hw3.js
@@ -1,7 +1,18 @@
-// With Date 2006-10-23
-//   Show => 23/10/2006
-//   Show => 2006-10-23 00:00:00
-//   Show => timestamp of this time
-//   Show => 23 October 2006
-//   Convert it time to persian calendar just with js method
+/*
+    - With Date 2006-10-23
+        Show => 23/10/2006
+        Show => 2006-10-23 00:00:00
+        Show => timestamp of this time
+        Show => 23 October 2006
+
+    - Convert it time to persian calendar just with js method
+*/
 // ----------------------------------------------------------------------------------------------------
+
+const date = new Date('2006-10-23');
+{
+    // Practice One
+}
+{
+    // Practice Two
+}
