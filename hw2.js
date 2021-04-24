bfbbe950368635a3608b66fa6bc0b2c30f21d30e
ehsan <ebaharinasab.eb@gmail.com>
2021-04-24 22:42:48 +0430

    start hw2

--- hw2.js
+++ hw2.js
@@ -1,8 +1,12 @@
-// Function that add a new course score and return the new user object (dynamic argument)
-// Function that add a new role and check if his age is under 18 add
-// young" to that object and return the object
-// status: "very-
-// Convert this object to array and after that add (fatherName: "ali") and then convert it to object
+/*
+    - Function that add a new course score and return the new user object (dynamic argument)
+
+    - Function that add a new role and check if his age is under 18 add status: "very-young"
+      to that object and return the object
+
+    - Convert this object to array and after that add (fatherName: "ali") and then convert
+      it to object
+*/
 // ----------------------------------------------------------------------------------------------------
 
 const user = {
@@ -13,3 +17,13 @@ const user = {
     age: 19,
     scores: {english: 10, math: 19.5, physics: 14},
 };
+
+{
+    // Practice One
+}
+{
+    // Practice Two
+}
+{
+    // Practice Three
+}
