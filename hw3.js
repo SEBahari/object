34eae91a7bf0560d769a421ed507f13a6d943359
ehsan <ebaharinasab.eb@gmail.com>
2021-04-25 05:28:39 +0430

    show a specific date in several formats

--- hw3.js
+++ hw3.js
@@ -12,6 +12,24 @@
 const date = new Date('2006-10-23');
 {
     // Practice One
+
+    console.log(date);
+    // 23/10/2006
+    console.log(date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear());
+
+    // 2006-10-23 3:30:0
+    console.log(
+        date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' '
+        + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds(),
+    );
+
+    // 1161561600000
+    console.log(+date);
+
+    // 23 October 2006
+    let options = {month: 'long'};
+    let month = new Intl.DateTimeFormat('en-US', options).format(date);
+    console.log(date.getDate() + ' ' + month + ' ' + date.getFullYear());
 }
 {
     // Practice Two
